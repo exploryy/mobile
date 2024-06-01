40f3ff55d48bbdb5b94f7b10a8215421e0216ad1
Alexander <73055263+cah999@users.noreply.github.com>
2024-06-02 02:04:06 +0700

    added pattern to map

--- app/src/main/java/com/example/explory/presentation/screen/map/MapScreen.kt
+++ app/src/main/java/com/example/explory/presentation/screen/map/MapScreen.kt
@@ -1,14 +1,9 @@
 package com.example.explory.presentation.screen.map
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,9 +17,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.explory.R
 import com.example.explory.data.model.Friend
@@ -39,9 +40,11 @@ import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.extension.compose.style.StyleImage
 import com.mapbox.maps.extension.compose.style.layers.generated.FillColor
 import com.mapbox.maps.extension.compose.style.layers.generated.FillLayer
 import com.mapbox.maps.extension.compose.style.layers.generated.FillOpacity
+import com.mapbox.maps.extension.compose.style.layers.generated.FillPattern
 import com.mapbox.maps.extension.compose.style.sources.generated.GeoJSONData
 import com.mapbox.maps.extension.compose.style.sources.generated.GeoJsonSourceState
 import com.mapbox.maps.extension.compose.style.standard.LightPreset
@@ -83,6 +86,12 @@ fun MapScreen(
         )
     )
 
+    val painter = painterResource(id = R.drawable.smoke)
+    val imageBitmap: ImageBitmap = remember(painter) {
+        painter.drawToBitmap()
+    }
+
+
     Box(Modifier.fillMaxSize()) {
         RequestLocationPermission(
             requestCount = mapState.permissionRequestCount,
@@ -111,7 +120,8 @@ fun MapScreen(
                                 sourceState = withHolesSourceState,
                                 layerId = OPENED_WORLD_LAYER,
                                 fillColor = FillColor(Color.DarkGray),
-                                fillOpacity = FillOpacity(1.0)
+                                fillOpacity = FillOpacity(1.0),
+                                fillPattern = FillPattern(StyleImage("fog", imageBitmap))
                             )
                         },
                         lightPreset = LightPreset.NIGHT
@@ -192,22 +202,18 @@ val friends = listOf(
     Friend("liiid", "давно не видели", R.drawable.picture, isBestFriend = false),
 )
 
-@RequiresApi(Build.VERSION_CODES.TIRAMISU)
-private fun Context.checkAndRequestNotificationPermission(
-    launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>,
-) {
-    val notificationPermission = arrayOf(
-        Manifest.permission.ACCESS_NOTIFICATION_POLICY,
-        Manifest.permission.POST_NOTIFICATIONS,
-    )
-    if (notificationPermission.all {
-            ContextCompat.checkSelfPermission(
-                this,
-                it
-            ) == PackageManager.PERMISSION_GRANTED
-        }
+
+fun Painter.drawToBitmap(): ImageBitmap {
+    val drawScope = CanvasDrawScope()
+    val bitmap = ImageBitmap(intrinsicSize.width.toInt(), intrinsicSize.height.toInt())
+    val canvas = Canvas(bitmap)
+    drawScope.draw(
+        density = Density(1f),
+        layoutDirection = LayoutDirection.Rtl,
+        canvas = canvas,
+        size = intrinsicSize
     ) {
-        return
+        draw(intrinsicSize)
     }
-    launcher.launch(notificationPermission)
+    return bitmap
 }
