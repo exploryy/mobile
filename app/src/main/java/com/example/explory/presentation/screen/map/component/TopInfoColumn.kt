68e662c3697123fe9528ba34a21b1eacf649eab5
Alexander <73055263+cah999@users.noreply.github.com>
2024-06-17 20:37:01 +0700

    added exp animation

--- app/src/main/java/com/example/explory/presentation/screen/map/component/TopInfoColumn.kt
+++ app/src/main/java/com/example/explory/presentation/screen/map/component/TopInfoColumn.kt
@@ -1,5 +1,6 @@
 package com.example.explory.presentation.screen.map.component
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -9,23 +10,30 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.explory.R
 import com.example.explory.ui.theme.AccentColor
 import com.example.explory.ui.theme.S16_W600
 import com.example.explory.ui.theme.S20_W600
 import com.example.explory.ui.theme.S24_W600
-import com.example.explory.ui.theme.White
+import kotlinx.coroutines.delay
 
 @Composable
 fun TopInfoColumn(
@@ -39,7 +47,7 @@ fun TopInfoColumn(
 ) {
     Row(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
+        verticalAlignment = Alignment.Top,
     ) {
         Column(Modifier.width(165.dp)) {
             Text(
@@ -69,9 +77,17 @@ fun TopInfoColumn(
 fun BalanceBar(
     modifier: Modifier = Modifier, balance: Int, level: Int, exp: Int, expToNextLevel: Int
 ) {
+    var isExpBarVisible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(exp) {
+        isExpBarVisible = true
+        delay(3000)
+        isExpBarVisible = false
+    }
+
     Column(
         modifier = modifier
-            .background(White.copy(0.1f), shape = RoundedCornerShape(8.dp))
+            .background(Color.White.copy(0.3f), shape = RoundedCornerShape(8.dp))
             .clip(RoundedCornerShape(8.dp))
             .padding(8.dp)
             .width(120.dp),
@@ -84,27 +100,30 @@ fun BalanceBar(
             Spacer(modifier = Modifier.weight(1f))
             BarItem(value = level, icon = R.drawable.star)
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        LinearProgressIndicator(
-            progress = { exp.toFloat() / expToNextLevel },
-            modifier = Modifier
-                .clip(RoundedCornerShape(8.dp)),
-            color = AccentColor,
-            trackColor = Color.White,
-        )
+        AnimatedVisibility(visible = isExpBarVisible) {
+            LinearProgressIndicator(
+                progress = { exp.toFloat() / expToNextLevel },
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                color = AccentColor,
+                trackColor = Color.White,
+            )
+        }
     }
 }
 
 @Composable
 fun BarItem(modifier: Modifier = Modifier, value: Int, icon: Int) {
     Row(
-        modifier = modifier,
+        modifier = modifier.widthIn(min = 0.dp, max = 60.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
             text = "$value",
             color = Color.White,
             style = S16_W600,
+            overflow = TextOverflow.Visible,
         )
         Spacer(modifier = Modifier.width(4.dp))
         Image(
